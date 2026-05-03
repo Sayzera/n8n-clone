699729a263e12ecf5848fb6251290af711be273c
Sayzera <white.code.text@gmail.com>
2026-05-04 00:27:05 +0300

    feat: update dependencies and implement AI functionality; modify .gitignore, enhance page component with AI mutation, and adjust TRPC router for AI execution

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 import { useTRPC } from "@/trpc/client";
 
+import { Logout } from "./logout";
+
 const Page = () => {
+  
   const trpc = useTRPC();
   const queryClient = useQueryClient();
   const { data } = useQuery(trpc.getWorkflows.queryOptions());
@@ -21,17 +25,44 @@ const Page = () => {
       onError: () => {},
     }),
   );
+
+
+
+  const testAi = useMutation(
+    trpc.testAi.mutationOptions({
+      onSuccess: (data) => {
+
+      },
+      onError: () => {
+
+      }
+    })
+  )
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server componet
       {JSON.stringify(data, null, 2)}
       <Button
+      disabled={testAi.isPending}
         onClick={() => {
           create.mutate();
         }}
       >
         Create Workflow
       </Button>
+
+      <Button
+       onClick={() => {
+        testAi.mutate()
+       }}
+      >
+        Work Gemini   
+
+      </Button>
+
+      <Logout />
+
     </div>
   );
 };
